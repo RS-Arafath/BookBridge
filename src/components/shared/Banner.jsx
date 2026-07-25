fa9452f69445f74dd4489a0a93b48e26f0a404ff
Arafath <rsarafath.signin@gmail.com>
2026-07-25 23:29:02 +0600

    feat: add image array to Banner component for dynamic content display

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -8,6 +8,12 @@ import 'swiper/css/effect-fade';
 import Link from 'next/link';
 import { Button } from '@heroui/react';
 
+const images = [
+  '/images/banner/banner-1.jpg',
+  '/images/banner/banner-2.jpg',
+  '/images/banner/banner-3.jpg',
+  '/images/banner/banner-5.jpg',
+];
 
 const Banner = () => {
   return (
