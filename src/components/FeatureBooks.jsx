e49e32bb0ee6e06642c5346dc6ab0436ef310079
Arafath <rsarafath.signin@gmail.com>
2026-07-31 17:41:16 +0600

    feat: update grid layout in FeatureBooks component for improved responsiveness on smaller screens

--- src/components/FeatureBooks.jsx
+++ src/components/FeatureBooks.jsx
@@ -25,7 +25,7 @@ export const FeatureBooks = async () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {featureData.map((item) => (
           <div
             key={item.id}
